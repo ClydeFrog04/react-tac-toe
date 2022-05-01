475e7976933392c4335e36ba22099fee38e9483e
ClydeFrog04 <egan.randal@gmail.com>
2022-05-01 15:26:49 -0600

    updated styles

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -13,13 +13,13 @@ const Cell = (props: CellProps) => {
     const x = props.position[0];
     const y = props.position[1];
 
-    const checkValidPlacement = () =>{
-        if(isGameOver) return false;
-        if(localBoard[x][y] !== ""){
+    const checkValidPlacement = () => {
+        if (isGameOver) return false;
+        if (localBoard[x][y] !== "") {
             return false;
         }
         return true;
-    }
+    };
     const isWinner = () => {
         if (localBoard[0][0] === localBoard[1][1] && localBoard[1][1] === localBoard[2][2] && localBoard[0][0] !== "")
             return true;
@@ -36,26 +36,27 @@ const Cell = (props: CellProps) => {
         else if (localBoard[1][0] === localBoard[1][1] && localBoard[1][1] === localBoard[1][2] && localBoard[1][0] !== "")
             return true;
         else return localBoard[2][0] === localBoard[2][1] && localBoard[2][1] === localBoard[2][2] && localBoard[2][0] !== "";
-    }
+    };
 
 
     const handleClick = (e: React.MouseEvent<HTMLSpanElement>) => {
-        if(checkValidPlacement()){
+        if (checkValidPlacement()) {
             const player = isXTurn ? "X" : "O";
             console.log("setting move");
             localBoard[x][y] = player;
             setBoard([...localBoard]);
-            if(isWinner()){
+            if (isWinner()) {
                 setIsGameOver(true);
                 console.log(`${player} wins!`);
             }
             setIsXTurn(!isXTurn);
         }
-    }
+    };
 
     return (
         <span className="cell" onClick={handleClick}>
             {props.children}
+
         </span>
     );
 };
